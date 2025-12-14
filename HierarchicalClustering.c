32301480d8afae739c3daccfd292f7534b49f67b
MICHMICHbob <zoe.malaise@student.uliege.be>
2025-12-14 17:13:29 +0100

    Update HierarchicalClustering.c

--- HierarchicalClustering.c
+++ HierarchicalClustering.c
@@ -1,10 +1,12 @@
 #ifndef HCLUS_H
 #define HCLUS_H
 
+#include <stdlib.h>
 #include "LinkedList.h"
 #include "BTree.h"
 #include "LinkedList.c"
 #include "BTree.c"
+
 
 struct Hclust_t
 {
@@ -13,14 +15,31 @@ struct Hclust_t
 };
 typedef struct 
 {
-  char *o; 
-  char *o'; 
+  char *o1; 
+  char *o2; 
   double dist; 
 } Triple;
 
 Hclust *hclustBuildTree(List *objects, double (*distFn)(const char *, const char *, void *), void *distFnParams)
 {
-  Triple tab []; // ini le tableau qui contient nos o, o' et notre distance
+  Hclust *clust = (Hclust *)malloc(sizeof(Hclust)); // alloc mémoire
+  if (clust==NULL)
+    return NULL; // si l'alloc de la mémoire échoue 
+  clust->dendro_tree = NULL ; //ini nos champs 
+  clust->nb_leaves = 0; 
+  Dict *dict = dictCreate(initial_dict_size); // on crée notre dictionnaire
+  if (dict==NULL) // notre alloc échoue
+    return 0;
+  char *objet_a = "a";
+// T_a est un pointeur vers un arbre BTree (votre cluster)
+BTree *T_a = btCreate(objet_a);
+}
+
+
+
+/*Hclust *hclustBuildTree(List *objects, double (*distFn)(const char *, const char *, void *), void *distFnParams)
+{
+  //Triple tab []; // ini le tableau qui contient nos o, o' et notre distance
   Node *current_node = llHead(objects);
   size_t initial_dict_size = llLength(objects) * 2; // on ini une taille 
   Dict *dict = dictCreate(initial_dict_size); // on crée notre dictionnaire
@@ -36,7 +55,7 @@ Hclust *hclustBuildTree(List *objects, double (*distFn)(const char *, const char
       dictInsert(dict, object, T_o);
       current_node = llNext(current_node);
     }
-
-
 }
+*/
+
 
