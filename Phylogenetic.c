e8d4d6b7b4c6ae44669bdd179b40d4b8a0a1b50f
Camille Kleber <C.kleber@student.uliege.be>
2025-12-18 15:54:32 +0100

    Update Phylogenetic.c

--- Phylogenetic.c
+++ Phylogenetic.c
@@ -1,1 +1,44 @@
+#include "HierarchicalClustering.h"
+#include "Phylogenetic.h"
 
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stlib.h>
+
+
+
+/*
+L’implémentation de Phylogenetic.c vous demandera d’écrire le code pour lire le fichier
+contenant les séquences et de préparer l’appel à hclustBuildTree. Pour cette implémentation, inspirez vous et adaptez le fichier main-features.c qui vous est fourni.
+*/
+/*calcule la distance d(o,o′) décrite dans l’équation (1) entre les deux séquences d’ADN données en argument. Si ces deux séquences n’ont pas la même longueur, on tronquera à droite la plus longue à la longueur
+de la plus courte.
+*/
+double phyloDNADistance(char *dna1, char *dna2){
+
+            int length1= strlen(dna1);
+            int length2= strlen(dna2);
+            int minlength = (length1>length2 )? length2 : length1;
+
+            int transitions=0;
+            int transversions =0;
+            for(int i=0; i< minlength; i++){ // on parcourt seulement la longueur du plus petit segment de gauche a droite (le plus long est tronque a droite)
+                        char c1 = dna1[i];
+                        char c2 = dna2[i];
+                        if(c1 != c2) // si les 2 lettres sont différentes
+                        {
+                                    if((c1 =='A'&& c2 =="G")|| (c1 =='G' && c2 =='A')||(c1 =='C' && c2 =='T')||(c1 =='T' && c2 =='C')){
+                                                transitions ++:
+                                    }
+                                    else{
+                                                tansversions ++;
+                                    }
+                        }
+            }
+            // P poproportion de transversions, Q de transition
+            double P = (double) transversions/minlength; 
+            double Q = (double) transitions/minlength;
+            double dist = (-(0.5*log(1-2*P -Q)) -(0.25)*log(1-2*Q)); 
+            return dist;
+}
